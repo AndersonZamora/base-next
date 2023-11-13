8fe3fe9cfa07227cc1abc478eb5d42b61c4b58b1
CinCoutTechnology <cincout.technology@gmail.com>
2023-11-12 20:46:40 -0500

    base next

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+
+interface Props {
+
+}
+
+const HomePage: NextPage<Props> = () => {
+    return (
+        <h1>Hola Loco Mundo</h1>
+    )
+}
+
+export default HomePage;
